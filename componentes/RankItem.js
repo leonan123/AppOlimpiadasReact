1f903cdb2a254442a21cb5c3b0c027bf270c59be
leonan123 <leonanorth3@gmail.com>
2021-08-10 21:54:07 -0300

    2 files modified

--- componentes/RankItem.js
+++ componentes/RankItem.js
@@ -4,15 +4,15 @@ export default function RankItem(props) {
   return (
     <div className={styles.item}>
       <div> {props.item.Rank} </div>
-      <div>
-        {/*  <img
+      <div className={styles.country}>
+        <img
           src={`https://olympics.com/tokyo-2020/olympic-games/resCOMMON/img/flags/${
             props.item.NOC
           }.png`}
-        /> */}
+        />
 
-        <img src="https://www.countryflags.io/br/flat/64.png" />
-        {props.item.Team}
+        {/*  <img src="https://www.countryflags.io/br/flat/64.png" />
+        {props.item.Team} */}
       </div>
       <div> {props.item.GoldMedals} </div>
       <div> {props.item.SilverMedals} </div>
