6d2622ddce91cbf44a8e5db476412ef293de335b
leonan123 <leonanorth3@gmail.com>
2021-08-17 20:12:02 -0300

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,21 @@ import react, { useState } from 'react';
 import dados from './dados.json';
 import styles from './index.module.css';
 import RankItem from '../componentes/RankItem.js';
+import { waitForDebugger } from 'inspector';
 
 export default function Home() {
   const [list, setList] = useState(dados);
 
+  function handleFilterByGold() {
+    const filteredList = [...list].filter(x => item.GoldMedals > 0);
+  }
   return (
     <div>
       <h1 className="title"> Quadro de medalhas - Tóquio 2020 </h1>
+      <div>
+        <button> Somente com Ouro </button>
+        <button> todos </button>
+      </div>
       <div> Resultado: {list.length}itens</div>
       <div>
         <div className={styles.header}>
